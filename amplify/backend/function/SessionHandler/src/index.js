ad120a0349576abf264b04fed8d93f0bd64fe9ac
adeyolus <seguncongrat@gmail.com>
2024-11-23 06:25:27 +0100

    firt commit
    itrak-acad-app first setup

---
+++ amplify/backend/function/SessionHandler/src/index.js
@@ -0,0 +1,19 @@
+
+//path = · /sessions/{sessionId}
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event) => {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    const sessionID = event.pathParameters.sessionId;
+    const session_data = {'sessionType': "End-User", 'session-code': "User-" + sessionID};
+    return {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+      headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "*"
+      },
+        body: JSON.stringify(session_data),
+    };
+};
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "api9f6ae8ba": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "SessionHandler": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
