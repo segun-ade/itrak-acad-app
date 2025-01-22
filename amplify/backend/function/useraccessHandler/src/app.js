3ee87e30e7eb2afee5ce1506f8aac74491b2ee58
adeyolus <seguncongrat@gmail.com>
2025-01-22 21:37:58 +0100

    expires date changed

--- amplify/backend/function/useraccessHandler/src/app.js
+++ amplify/backend/function/useraccessHandler/src/app.js
@@ -210,7 +210,7 @@ app.get('/checkreguser', (req, res) => {
                               req.session.userid = req.query.user_id;
                               const sessiontoken = uuid.v4();
                               req.session.token = sessiontoken;
-                              res.cookie("session_token", sessiontoken, {maxAge:900000});
+                              res.cookie("session_token", sessiontoken, {maxAge:90000000});
                               console.log(sessiontoken);
                           }
                           //req.session.user = conresult;
