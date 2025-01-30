f009fcfb733645bbae4edea0ef8fdc0ac531600b
adeyolus <seguncongrat@gmail.com>
2025-01-30 19:05:15 +0100

    cors2

--- amplify/backend/function/useraccessHandler/src/app.js
+++ amplify/backend/function/useraccessHandler/src/app.js
@@ -68,6 +68,7 @@ app.use(
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", 'https://www.itraktech.com')
   res.header("Access-Control-Allow-Headers", 'https://www.itraktech.com')
+  res.header("Access-Control-Allow-Credentials", true)
   next()
 });
 
