9b060fb3a2604791efbc947744cb8accbf08c1be
adeyolus <seguncongrat@gmail.com>
2025-01-31 16:32:20 +0100

    cors

--- amplify/backend/function/useraccessHandler/src/app.js
+++ amplify/backend/function/useraccessHandler/src/app.js
@@ -74,7 +74,7 @@ app.use(
 
 // Enable CORS for all methods
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", ['https://www.itraktech.com','https://main.d30qj1a33kc2he.amplifyapp.com'])
+  res.header("Access-Control-Allow-Origin", 'https://www.itraktech.com')
   res.header("Access-Control-Allow-Headers", '*')
   res.header("Access-Control-Allow-Credentials", true)
   next()
