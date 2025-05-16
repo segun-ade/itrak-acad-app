486f86179fde96b60f562157e9a772a49d2c26a4
adeyolus <seguncongrat@gmail.com>
2025-05-16 21:02:26 +0100

    add flask routes

--- amplify/backend/function/fileDBupdater/src/index.py
+++ amplify/backend/function/fileDBupdater/src/index.py
@@ -5,20 +5,45 @@ from flask_cors import CORS
 from flask import Flask, jsonify, request
 from uuid import uuid4
 
+DB_BASE_ROUTE = "/updateDB/students/{school}/{session}/{class}"
+FILE_BASE_ROUTE = "/updateFile/students/{school}/{session}/{class}"
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route(DB_BASE_ROUTE, methods=['POST'])
+def fileToDB():
+  return jsonify(message="Students record file successfully written to database!", method="POST")
+
+@app.route(DB_BASE_ROUTE, methods=['GET'])
+def fileToDB():
+  return jsonify(message="Students record file successfully written to database!", method="GET")
+
+@app.route(FILE_BASE_ROUTE, methods=['POST'])
+def DBToFile():
+  return jsonify(message="Students data record successfully written to file!", method="POST")
+
+@app.route(FILE_BASE_ROUTE, methods=['GET'])
+def DBToFile():
+  return jsonify(message="Students data record successfully written to file!", method="GET")
+
 def handler(event, context):
-  print('received event:')
-  print(event)
-  #school = event['pathParameters']
-  #print(event['pathParameters'])
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda! Welcome to iTrak!')
-  }
+  return awsgi.response(app, event, context)
+
+#def handler(event, context):
+#  print('received event:')
+#  print(event)
+#  #school = event['pathParameters']
+#  #print(event['pathParameters'])
+#  return {
+#      'statusCode': 200,
+#      'headers': {
+#          'Access-Control-Allow-Headers': '*',
+#          'Access-Control-Allow-Origin': '*',
+#          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+#      },
+#      'body': json.dumps('Hello from your new Amplify Python lambda! Welcome to iTrak!')
+#  }
 
 #aws-wsgi = "*"
 #boto3 = "*"
