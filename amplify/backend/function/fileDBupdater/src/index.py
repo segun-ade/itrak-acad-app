acc30becaaf225d4f350257f6b068fac372f74ef
adeyolus <seguncongrat@gmail.com>
2025-05-24 22:10:41 +0100

    import fileupdater

--- amplify/backend/function/fileDBupdater/src/index.py
+++ amplify/backend/function/fileDBupdater/src/index.py
@@ -4,10 +4,10 @@ import awsgi
 from flask_cors import CORS
 from flask import Flask, jsonify, request
 from uuid import uuid4
-#import fileupdater as fDBWriter
-import pandas as pd
-import mysql.connector as mysql
-from openpyxl import load_workbook
+import fileupdater as fDBWriter
+#import pandas as pd
+#import mysql.connector as mysql
+#from openpyxl import load_workbook
 
 DB_BASE_ROUTE = "/updateDB/students/{school}/{session}/{class}"
 FILE_BASE_ROUTE = "/updateFile/students/{school}/{session}/{class}"
@@ -25,15 +25,26 @@ def postFileToDB():
 
 @app.route(DB_BASE_ROUTE, methods=['GET'])
 def getFileToDB():
-  #fDBWriter.read_record_sheet("SENSYCAMv2_DHE updated.xlsm","SENSYCAMv2")
+  fDBWriter.read_record_sheet("SENSYCAMv2_DHE updated.xlsm","SENSYCAMv2")
   return jsonify(message="Students record file successfully written to database!", method="GET", school=req_school, session=req_session, student_class=req_class)
 
 @app.route(FILE_BASE_ROUTE, methods =['POST'])
 def postDBToFile():
+  fDBWriter.create_stdt_workbook("-class performance",False,False,34547,req_school,"*","*")
+  #create_stdt_workbook("-class performance",True,False,34547,'BRT0002A',"*","*")
+  #create_stdt_workbook("-student performance",True,False,34547,'CHF0003T','adeypatb0003',"*")
+  #create_stdt_workbook("-admin record",False,False,63628,'MSB0001A',"*","*")
+  fDBWriter.create_stdt_workbook("-class attendance",False,False,59405,'MSB0001A',"*","*")
+  fDBWriter.create_stdt_workbook("-class attendance",True,False,59405,'MSB0001A',"*","*")
+  #create_stdt_workbook("-school form",False,True,0,'MSB0001A',"*","*")
+  #create_stdt_workbook("-school record",True,True,0,'MSB0001A',"*","*")
+  #create_stdt_workbook("-schools form",False,True,0,"*","*","*")
+  #create_stdt_workbook("-schools record",True,True,0,"*","*","*")
   return jsonify(message="Students data record successfully written to file!", method="POST", school=req_school, session=req_session, student_class=req_class)
 
 @app.route(FILE_BASE_ROUTE, methods=['GET'])
 def getDBToFile():
+  fDBWriter.update_record_sheet("MSB0001A-class attendance.xlsx","Attendance",record_data)
   return jsonify(message="Students data record successfully written to file!", method="GET", school=req_school, session=req_session, student_class=req_class)
 
 def handler(event, context):
@@ -44,7 +55,9 @@ def handler(event, context):
   req_school = event['pathParameters']['school']
   req_class = event['pathParameters']['class']
   req_session = event['pathParameters']['session']
-  print('school: '+req_school+', session: '+req_session+', class: '+req_class)
+  global record_data
+  record_data = 'school: '+req_school+', session: '+req_session+', class: '+req_class
+  print(record_data)
   event['httpMethod'] = event['httpMethod']
   event['path'] = event['resource']#'updateDB/students/{school}/{session}/{class}'
   if event['queryStringParameters']=='None':
