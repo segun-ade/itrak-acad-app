1c30bc676c2ee771e2c1ba8ce3e1e4772abae56b
adeyolus <seguncongrat@gmail.com>
2025-08-18 04:17:06 +0100

    update string

--- amplify/backend/function/fileDBupdater/src/index.py
+++ amplify/backend/function/fileDBupdater/src/index.py
@@ -365,8 +365,8 @@ def postFileToDB():
       insert_query = ''
       for value in insert_data[i].values():
         insert_query += '\'' + value + '\'' + ','
-        insert_query =  insert_query.replace(",)", ")")
       insert_string = f'INSERT INTO {req_act_type} VALUES ({insert_query})'
+      insert_query =  insert_query.replace(",)", ")")
       print(insert_string)
       concursor.execute(insert_string)
       result = concursor.fetchall() #data workbook
